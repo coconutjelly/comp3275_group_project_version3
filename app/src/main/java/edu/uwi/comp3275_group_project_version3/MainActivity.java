fad4bc01e4b2e3a4d5b60dbb8139df5e5c9cf180
coconutjelly7 <coconutjelly7@gmail.com>
2018-04-08 13:58:28 -0400

    Initial commit

---
+++ app/src/main/java/edu/uwi/comp3275_group_project_version3/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.uwi.comp3275_group_project_version3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
